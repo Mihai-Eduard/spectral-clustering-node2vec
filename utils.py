45ce25c7e4fe1b09f54e263a1a273ef4bfd2a277
Eduard Alex Mihai <E.A.Mihai@student.tudelft.nl>
2024-02-24 13:20:40 +0100

    Keep just the files for Kaggle

--- utils.py
+++ utils.py
@@ -1,51 +1,59 @@
+from typing import List, Dict
+import pandas as pd
 import networkx as nx
-import numpy as np
-import random
 
 
-def random_walks(graph: nx.Graph, num_walks: int, walk_length: int) -> np.ndarray:
-    """Perform random walks on an unweighted graph.
-
-    Args:
-        graph (nx.Graph): The graph.
-        num_walks (int): The number of random walks for each node.
-        walk_length (int): The number of nodes in a random walk.
-
+def get_edges(data: pd.DataFrame, nodes) -> List:
+    """ Given the dataframe with articles and lists return the set of edges
+        Args:
+        data (pd.DataFrame): The medium dataset
+        nodes: dict (node_id: article title)
     Returns:
-        np.ndarray: The random walks, shape (n_nodes * num_walks, walk_length)
-    """
-    nodes = list(graph.nodes())
-    result = np.zeros((len(nodes) * num_walks, walk_length))
+            edges (List[tuple]): List of edges"""
+    edges = []
+    buckets = {}
+    for index, row in data.iterrows():
+        if index not in nodes:
+            continue
+        for current_list in row["list"].split("; "):
+            if current_list not in buckets:
+                buckets[current_list] = []
+            buckets[current_list].append(index)
+    print(f'Number of buckets: {len(buckets)}.')
 
-    for i, node in enumerate(nodes):
-        print(f"Random walks for node {i + 1}/{len(nodes)}", end="\r")
-        for j in range(num_walks):
-            current = node
-            for k in range(walk_length):
-                current = random.choice(list(graph.neighbors(current)))
-                result[i * num_walks + j, k] = current
-
-    return result
+    for key in buckets:
+        similar_nodes = buckets[key]
+        for i in range(len(similar_nodes)):
+            for j in range(i + 1, len(similar_nodes)):
+                edges.append((similar_nodes[i], similar_nodes[j]))
+    return edges
 
 
-def compute_spectral_embeddings(graph: nx.Graph, dim: int) -> np.ndarray:
-    """Perform spectral clustering on the graph and compute low-dimensional node representations.
-    Does not normalize the Laplacian.
-
-    Args:
-        graph (nx.Graph): The graph.
-        dim (int): The dimension of representations. This corresponds to the number of eigenvectors used.
-
+def get_nodes(data: pd.DataFrame) -> Dict:
+    """ Given the dataframe with articles and lists return the set of nodes
+        Args:
+        data (pd.DataFrame): The medium dataset
     Returns:
-        np.ndarray: Node representations (sorted by node ID, ascending), shape (num_nodes, dim).
+        nodes: dict (node_id: article title)"""
+    nodes = {}
+    for index, row in data.iterrows():
+        if index not in nodes:
+            nodes[index] = row["title"]
+    print(len(nodes))
+    return nodes
+
+
+def form_graph(data: pd.DataFrame) -> nx.Graph:
+    """Forms graph from medium article dataset.
+    Args:
+        data (pd.DataFrame): The medium dataset
+    Returns:
+        G (nx.Graph): The graph.
     """
-    adjacency_matrix = nx.to_numpy_array(graph, nodelist=sorted(graph.nodes))
-
-    # make sure the matrix is symmetric
-    assert (adjacency_matrix == adjacency_matrix.T).all()
-
-    laplacian_matrix = np.diag(np.sum(adjacency_matrix, axis=1)) - adjacency_matrix
-    eigen_results = np.linalg.eigh(laplacian_matrix)
-    result = eigen_results[1][:, :dim]
-
-    return result
+    texts = [x[0] + " " + x[1] for x in zip(data.title, data.subtitle)]
+    nodes = get_nodes(data)
+    edges = get_edges(data, nodes)
+    graph = nx.Graph()
+    graph.add_nodes_from(list(nodes.keys()))
+    graph.add_edges_from(edges)
+    return graph
